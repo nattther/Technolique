5ac8ae37003323f4f151eb90063ce0a0b3fb2382
Nathan <n.tournant@myges.fr>
2023-12-12 19:18:46 +0100

    Adding the copy of Gaetan project for a base.

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'app_default')]
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', ['name' => 'tata']);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+}
